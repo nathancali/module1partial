3fa09e00bef9ffe4e73f3667af833e296d2985f5
Nathan Calinisan <nathancali30@gmail.com>
2023-03-22 14:58:34 +0800

    Add project files.

---
+++ module1partial/item.cs
@@ -0,0 +1,10 @@
+﻿namespace module1partial
+{
+    public class item
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public string Brand { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
